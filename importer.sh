90fc35fbebe82382e7d0befb306dbd0c4ef508d4
Adam Hamot <AdamHamot@gmail.com>
2016-02-05 01:13:43 -0500

    Added a directory structure

--- importer.sh
+++ importer.sh
@@ -1,6 +1,7 @@
 #/bin/bash
 
 basedir=~
+confdir=config_files
 progname=`basename "$0"`
 files=`ls -aI $progname -I . -I .. -I .git`
 for f in $files
@@ -8,17 +9,17 @@ do
   if [[ -a "$basedir/$f" ]]
   then
     echo -e "\e[31m$basedir/$f\e[0m currently exists, do you want to diff the two?"
-    echo -e "(\e[32mY\e[0m): Will run: \e[32mvimdiff $basedir/$f $f\e[0m"
+    echo -e "(\e[32mY\e[0m): Will run: \e[32mvimdiff $confdir/f $basedir/$f\e[0m"
     echo "(n): To continue linking.."
     read ans
     if [[ (${ans:0:1} == "y") || (${ans:0:1} == "") ]]
     then
-      vimdiff $basedir/$f $f
+      vimdiff $confdir/$f $basedir/$f
       echo "Resolved? (Y/n)"
       read resolved
       if [[ (${resolved:0:1} == "y") || (${resolved:0:1} == "") ]]
       then
-        ln -s "$f" "$basedir/$f"
+        ln -s "$confdir/$f" "$basedir/$f"
       else
         echo "Moving on.."
       fi
