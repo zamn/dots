8f5e8979802fedc6e9f75012d398915c205f3af0
Adam Hamot <adam@zamn.net>
2020-04-29 20:42:57 -0400

    Pimp out importer script, use config token properly.. hopefully

--- importer.sh
+++ importer.sh
@@ -8,8 +8,25 @@ then
   exit 1
 fi
 
+if [[ -z "$PINEENTRY_PROGRAM" ]]
+then
+    echo "You *MUST* set \$PINEENTRY_PROGRAM in .keys.bash (at the least) to get stuff working."
+    exit 1234
+fi
+
 # TODO: Make this less hacky
-sed "s/\$\PINEENTRY_PROGRAM/$(eval echo $PINEENTRY_PROGRAM | sed 's/\//\\\//g')/g" gnupg/.gnupg/gpg-agent.conf
+if [[ "$platform" != "Linux" ]]
+then
+    sed -i "s/\$\PINEENTRY_PROGRAM/$(eval echo $PINEENTRY_PROGRAM | sed 's/\//\\\//g')/g" gnupg/.gnupg/gpg-agent.conf
+else
+    which gsed 2>&1 >/dev/null
+    if [[ `echo $?` -ne 0 ]]
+    then
+        echo "You must have gsed installed because macs are garbage and ship with a super old version of sed that does not support inline replacement."
+    else
+        gsed -i "s/\$\PINEENTRY_PROGRAM/$(eval echo $PINEENTRY_PROGRAM | sed 's/\//\\\//g')/g" gnupg/.gnupg/gpg-agent.conf
+    fi
+fi
 
 # stow all files in top level dirs
 for d in `ls -d */ | tr / ' '`
