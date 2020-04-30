b674a823fb7b5ceae9d5380a84ad1d97496a7a73
Adam Hamot <adam@zamn.net>
2020-04-29 21:28:07 -0400

    Only load ident files on demand

--- bash/.linux.bash
+++ bash/.linux.bash
@@ -7,10 +7,9 @@ source /usr/share/nvm/init-nvm.sh
 has_keychain=`which keychain >/dev/null; echo $?`
 if [[ "$has_keychain" -eq 0 ]]
 then
-    IDENT_FILES=$(grep "IdentityFile" ~/.ssh/config | uniq | awk '{print $2}')
     WORK_GPG_KEY=$(grep "signingkey" ~/work/.gitconfig | awk -F= '{print $2}')
     CODE_GPG_KEY=$(grep "signingkey" ~/code/.gitconfig | awk -F= '{print $2}')
-    eval `keychain -q --eval --nogui -Q --gpg2 --agents ssh,gpg $IDENT_FILES $WORK_GPG_KEY $CODE_GPG_KEY`
+    eval `keychain -q --eval --nogui -Q --gpg2 --agents ssh,gpg $WORK_GPG_KEY $CODE_GPG_KEY`
 fi
 
 export PATH=$PATH:/usr/bin/core_perl:~/.bin
