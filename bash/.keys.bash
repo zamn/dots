dee44c9d9c5b1aae8b83255ac47c2f16a4e485c3
Adam Hamot <adam@zamn.net>
2020-09-14 19:03:27 -0400

    Some cool bash changes :^)

--- bash/.keys.bash
+++ bash/.keys.bash
@@ -2,3 +2,4 @@ export FUGITIVE_GITLAB_PAT=''
 export PINEENTRY_PROGRAM=''
 export NPM_CONFIG_TOKEN=''
 export GITLAB_READONLY_PAT=''
+export PAGER=less
--- bash/.linux.bash
+++ bash/.linux.bash
@@ -12,3 +12,5 @@ then
 fi
 
 export PATH=$PATH:/usr/bin/core_perl:~/.bin
+# Save and reload the history after each command finishes
+export PROMPT_COMMAND="history -a; history -c; history -r; $PROMPT_COMMAND"
