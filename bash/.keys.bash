f5d2ee66883918b827fbd7dc2fe4f03c7aa5e282
Adam Hamot <adam@zamn.net>
2021-03-22 19:13:13 -0400

    latest innovations

--- bash/.keys.bash
+++ bash/.keys.bash
@@ -1,5 +1,6 @@
 export FUGITIVE_GITLAB_PAT=''
 export PINEENTRY_PROGRAM=''
 export NPM_CONFIG_TOKEN=''
-export GITLAB_READONLY_PAT=''
+export GITLAB_API_PAT=''
 export PAGER=less
+export VPN=''
--- bash/.linux.bash
+++ bash/.linux.bash
@@ -8,9 +8,24 @@ if [[ "$has_keychain" -eq 0 ]]
 then
     WORK_GPG_KEY=$(grep "signingkey" ~/work/.gitconfig | awk -F= '{print $2}')
     CODE_GPG_KEY=$(grep "signingkey" ~/code/.gitconfig | awk -F= '{print $2}')
-    eval `keychain -q --eval --nogui -Q --gpg2 --agents ssh,gpg $WORK_GPG_KEY $CODE_GPG_KEY`
+    echo $WORK_GPG_KEY $CODE_GPG_KEY
+    eval `keychain -q --eval --nogui -Q --gpg2 --eval id_rsa --agents ssh,gpg $WORK_GPG_KEY $CODE_GPG_KEY`
 fi
 
 export PATH=$PATH:/usr/bin/core_perl:~/.bin
 # Save and reload the history after each command finishes
 export PROMPT_COMMAND="history -a; history -c; history -r; $PROMPT_COMMAND"
+
+mm_vpn() {
+    if [[ "$1" = "d" ]]
+    then
+        ssh mac /opt/cisco/anyconnect/bin/vpn disconnect
+    else
+        ssh mac /opt/cisco/anyconnect/bin/vpn connect "$VPN"
+    fi
+}
+
+export -f mm_vpn
+
+# Probably should go in login shell, but yolo
+xmodmap ~/.Xmodmap
